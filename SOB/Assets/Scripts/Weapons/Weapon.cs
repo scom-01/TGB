05f1e9a473964fac1b9d94be3ab9367a9eec2914
KDH <ekgns0131@daum.net>
2023-01-14 21:39:28 +0900

    fix: Weapon / 타이머 추가, WeaponAnimationToWeapon 을 대신하는 AnimationEventHandler로 Trigger 설정

--- SOB/Assets/Scripts/Weapons/Weapon.cs
+++ SOB/Assets/Scripts/Weapons/Weapon.cs
@@ -1,173 +1,143 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Timers;
 using UnityEngine;
+using SOB.CoreSystem;
+using SOB.Utilities;
 
-public class Weapon : MonoBehaviour
+namespace SOB.Weapons
 {
-    [field: SerializeField] public SO_WeaponData weaponData { get; private set; }
-    [SerializeField] private float attackCounterResetCooldown;
-
-    [HideInInspector]
-    protected string weaponAnimBoolStr;
-
-    [Tooltip("공격 횟수")]
-    public int CurrentAttackCounter
+    public class Weapon : MonoBehaviour
     {
-        get => currentAttackCounter;
-        private set => currentAttackCounter = value >= weaponData.amountOfAttacks ? 0 : value;
-    }
+        [field: SerializeField] public WeaponDataSO weaponData { get; private set; }
+        [SerializeField] private float attackCounterResetCooldown;
 
-    public Core core { get; private set; }
+        [HideInInspector]
+        protected string weaponAnimBoolStr;
 
-    private int currentAttackCounter;
-    private Timer attackCounterResetTimer;
-
-    public event Action OnEnter;
-    public event Action OnExit;
-
-    protected Animator baseAnimator;
-    public GameObject BaseGameObject { get; private set; }    
-    protected Animator WeaponAnimator;
-    public GameObject WeaponSpriteGameObject { get; private set; }
-
-    public AnimationEventHandler EventHandler { get; private set; }
-
-    [HideInInspector]
-    protected WeaponManager weaponManager;
-
-    [HideInInspector]
-    public bool InAir;
-
-    protected PlayerWeaponState state;
-    protected virtual void Awake()
-    {
-        BaseGameObject = transform.Find("Base").gameObject;
-        WeaponSpriteGameObject = transform.Find("Weapon").gameObject;
-        baseAnimator = transform.Find("Base").GetComponent<Animator>();
-        WeaponAnimator = transform.Find("Weapon").GetComponent<Animator>();
-
-        weaponManager = GetComponentInParent<WeaponManager>();
-        //attackCounterResetTimer = new Timer(attackCounterResetCooldown);
-    }
-
-    private void Update()
-    {
-        //attackCounterResetTimer.Tick();
-    }
-
-    private void OnEnable()
-    {
-        EventHandler.OnFinish += ExitWeapon;
-        //attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
-    }
-
-    private void OnDisable()
-    {
-        EventHandler.OnFinish -= ExitWeapon;
-        //attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
-    }
-
-    /// <summary>
-    /// PlayerWeaponState Enter 에서 호출되는 함수
-    /// </summary>
-    public virtual void EnterWeapon()
-    {
-        gameObject.SetActive(true);
-
-        //attackCounterResetTimer.StopTimer();
-
-        if (InAir)
+        [Tooltip("공격 횟수")]
+        public int CurrentAttackCounter
         {
-            SetBoolName("inAir", true);            
-        }
-        else
-        {
-            SetBoolName("inAir", false);
+            get => currentAttackCounter;
+            private set => currentAttackCounter = value >= weaponData.NumberOfAttacks ? 0 : value;
         }
 
-        OnEnter?.Invoke();
+        public Core WeaponCore { get; private set; }
 
-        weaponManager.lastAttackTime = Time.time;
+        private int currentAttackCounter;
+        private Timer attackCounterResetTimer;
+
+        public event Action OnEnter;
+        public event Action OnExit;
+
+        protected Animator baseAnimator;
+        public GameObject BaseGameObject { get; private set; }
+        protected Animator WeaponAnimator;
+        public GameObject WeaponSpriteGameObject { get; private set; }
+
+        public AnimationEventHandler EventHandler { get; private set; }
+
+        [HideInInspector]
+        public bool InAir;
+
+        protected PlayerWeaponState state;
+        protected virtual void Awake()
+        {
+            BaseGameObject = transform.Find("Base").gameObject;
+            WeaponSpriteGameObject = transform.Find("Weapon").gameObject;
+
+            baseAnimator = transform.Find("Base").GetComponent<Animator>();
+
+            EventHandler = BaseGameObject.GetComponent<AnimationEventHandler>();
+            
+            attackCounterResetTimer = new Timer(attackCounterResetCooldown);
+        }
+
+        private void Update()
+        {
+            attackCounterResetTimer.Tick();
+        }
+
+        private void OnEnable()
+        {
+            EventHandler.OnFinish += ExitWeapon;
+            attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
+        }
+
+        private void OnDisable()
+        {
+            EventHandler.OnFinish -= ExitWeapon;
+            attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
+        }
+
+        /// <summary>
+        /// PlayerWeaponState Enter 에서 호출되는 함수
+        /// </summary>
+        public virtual void EnterWeapon()
+        {
+            print($"{transform.name} enter");
+
+            gameObject.SetActive(true);
+
+            attackCounterResetTimer.StopTimer();
+                        
+            SetBoolName("inAir", InAir);            
+
+            OnEnter?.Invoke();
+
+        }
+
+        /// <summary>
+        /// PlayerWeaponState Exit 에서 호출되는 함수
+        /// </summary>
+        public virtual void ExitWeapon()
+        {
+            BaseGameObject.GetComponent<SpriteRenderer>().sprite = null;
+            WeaponSpriteGameObject.GetComponent<SpriteRenderer>().sprite = null;
+            gameObject.SetActive(false);
+
+            OnExit?.Invoke();
+        }
+
+        public void SetCore(Core core)
+        {
+            WeaponCore = core;
+        }
+
+        public void ChangeAttackCounter(int value)
+        {
+            CurrentAttackCounter = value;
+        }
+
+        public void ResetAttackCounter()
+        {
+            CurrentAttackCounter = 0;
+        }
+
+        #region Set Func
+
+        /// <summary>
+        /// Animator Parameter Set Func
+        /// </summary>
+        /// <param name="name"> Param Name</param>
+        /// <param name="setbool">T, F</param>
+        public virtual void SetBoolName(string name, bool setbool)
+        {
+            weaponAnimBoolStr = name;
+            baseAnimator.SetBool(name, setbool);
+            //WeaponAnimator.SetBool(name, setbool);
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 초기 State 설정
+        /// </summary>
+        /// <param name="state"></param>
+        public void InitializeWeapon(PlayerWeaponState state)
+        {
+            this.state = state;
+        }
     }
 
-    /// <summary>
-    /// PlayerWeaponState Exit 에서 호출되는 함수
-    /// </summary>
-    public virtual void ExitWeapon()
-    {
-        BaseGameObject.GetComponent<SpriteRenderer>().sprite = null;
-        WeaponSpriteGameObject.GetComponent<SpriteRenderer>().sprite = null;
-        gameObject.SetActive(false);
-
-        OnExit?.Invoke();
-    }
-
-    public void ChangeAttackCounter(int value)
-    {
-        CurrentAttackCounter = value;
-    }
-
-    public void ResetAttackCounter()
-    {
-        CurrentAttackCounter = 0;
-    }
-
-    #region Set Func
-
-    /// <summary>
-    /// Animator Parameter Set Func
-    /// </summary>
-    /// <param name="name"> Param Name</param>
-    /// <param name="setbool">T, F</param>
-    public virtual void SetBoolName(string name, bool setbool)
-    {
-        weaponAnimBoolStr = name;
-        baseAnimator.SetBool(name, setbool);
-        //WeaponAnimator.SetBool(name, setbool);
-    }
-    #endregion
-
-    #region Animation Triggers
-    public virtual void AnimationFinishTrigger()
-    {
-        state.AnimationFinishTrigger();
-    }
-
-    public virtual void AnimationStartMovementTrigger()
-    {
-        //state.SetPlayerVelocity(weaponData.movementSpeed[CurrentAttackCounter]);
-    }
-
-    public virtual void AnimationStopMovementTrigger()
-    {
-        state.SetPlayerVelocity(0f);
-    }
-
-    public virtual void AnimationTurnOnFlipTrigger()
-    {
-        state.SetFlipCheck(true);
-    }
-
-    public virtual void AnimationTurnOffFlipTrigger()
-    {
-        state.SetFlipCheck(false);
-    }
-
-    public virtual void AnimationActionTrigger()
-    {
-
-    }
-    
-    #endregion
-
-    /// <summary>
-    /// 초기 State 설정
-    /// </summary>
-    /// <param name="state"></param>
-    public void InitializeWeapon(PlayerWeaponState state)
-    {
-        this.state = state;
-    }
 }
