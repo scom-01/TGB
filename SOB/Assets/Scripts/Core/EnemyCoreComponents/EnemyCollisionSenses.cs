8e3950150b98be0c0f28f72619c48bb8fe0cf4ca
KDH <ekgns0131@daum.net>
2023-02-05 08:34:11 +0900

    fix: PlayerDetected fix(1)

--- SOB/Assets/Scripts/Core/EnemyCoreComponents/EnemyCollisionSenses.cs
+++ SOB/Assets/Scripts/Core/EnemyCoreComponents/EnemyCollisionSenses.cs
@@ -30,6 +30,12 @@ public class EnemyCollisionSenses : CollisionSenses
 
     public bool PlayerDectected
     {
-        get => Physics2D.Raycast(transform.position, Vector2.right * EnemyMovement.FancingDirection, enemy.enemyData.playerDetectedDistance, WhatIsPlayer);
+        get
+        {
+            RaycastHit2D ray1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.5f), Vector2.right * EnemyMovement.FancingDirection, enemy.enemyData.playerDetectedDistance, WhatIsPlayer);
+            RaycastHit2D ray2 = Physics2D.Raycast(transform.position, Vector2.right * EnemyMovement.FancingDirection, enemy.enemyData.playerDetectedDistance, WhatIsPlayer);
+            RaycastHit2D ray3 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.5f), Vector2.right * EnemyMovement.FancingDirection, enemy.enemyData.playerDetectedDistance, WhatIsPlayer);
+            return (ray1 || ray2 || ray3);
+        }
     }
 }
