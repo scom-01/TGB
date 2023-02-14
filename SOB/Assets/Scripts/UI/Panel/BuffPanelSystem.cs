c368be4a00df2ea7e8e2a18e80dd0dd20b07fc6f
KDH <ekgns0131@daum.net>
2023-02-15 03:39:23 +0900

    Make BuffPanelSystem GridLayout Object

--- SOB/Assets/Scripts/UI/Panel/BuffPanelSystem.cs
+++ SOB/Assets/Scripts/UI/Panel/BuffPanelSystem.cs
@@ -1,67 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class BuffPanelSystem : MonoBehaviour
 {
-    private Player player;
-
-    public List<Buff> buffs;
-    [SerializeField] private GameObject buffVertical;
-
     [Header("Prefab")]
-    [SerializeField] private GameObject buffPanelHorizontalPrefab;
     [SerializeField] private GameObject buffPanelPrefab;
 
-    [SerializeField] private int MaxBuffPanelHorizontal;
+    //[SerializeField] private int MaxBuffPanelHorizontal;
 
     private void Awake()
     {
-        this.player = GameManager.Inst.player.GetComponent<Player>();
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-
+        //GetComponent<GridLayoutGroup>().constraintCount = MaxBuffPanelHorizontal;
     }
 
     public void BuffPanelAdd(Buff buff)
     {
-        if(buffVertical.GetComponentsInChildren<BuffPanelItem>().Length > 0)
-        {
-
-        }
-        else
-        {
-            GameObject horizontal = Instantiate(buffPanelHorizontalPrefab, buffVertical.transform);
-            GameObject item = Instantiate(buffPanelPrefab, horizontal.transform);
-            //item.GetComponent<BuffPanelItem>().BuffDurationTime = buff.BuffItemData.DurationTime;
-        }
-
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        BuffPanelUpdate();
-    }
-
-    public void BuffPanelUpdate()
-    {
-        for (int i = 0; i < buffs.Count; i++)
-        {            
-            if (Time.time >= buffs[i].durationTime + buffs[i].startTime)
-            {
-                //buffs[i].
-            }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-
+        BuffPanelItem item = Instantiate(buffPanelPrefab, this.transform).GetComponent<BuffPanelItem>();
+        item.buff = buff;
     }
 }
