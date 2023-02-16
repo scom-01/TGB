1c0ff72d90c75e0afaa033f5253363b32f6c6dd4
KDH <ekgns0131@daum.net>
2023-02-16 19:35:22 +0900

    Fix RandomParticleInstantiate in DamageReceiver.Damage

--- SOB/Assets/Scripts/Core/CoreComponents/DamageReceiver.cs
+++ SOB/Assets/Scripts/Core/CoreComponents/DamageReceiver.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
 using TMPro;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
@@ -7,45 +9,73 @@ namespace SOB.CoreSystem
 {
     public class DamageReceiver : CoreComponent, IDamageable
     {
+        public GameObject DefaultEffectPrefab;
+
         private CoreComp<UnitStats> stats;
         private CoreComp<ParticleManager> particleManager;
-        public void Damage(GameObject attacker, GameObject victim, ElementalPower elementalPower, DamageAttiribute attiribute, float amount)
-        {
-            //attacker.GetComponentInChildren<Core>().GetCoreComponent<UnitStats>()
-            Debug.Log(core.transform.parent.name + " " + amount + " Damaged!");
-            stats.Comp?.DecreaseHealth(elementalPower, attiribute, amount);            
-            //if (damageParticles == null)
-            //{
-            //    Debug.Log("Combat DamageParticles is Null");
-            //    return;
-            //}
-            //particleManager.Comp?.StartParticlesWithRandomRotation(damageParticles);
-        }
+
+        //public void Damage(GameObject attacker, GameObject victim, ElementalPower elementalPower, DamageAttiribute attiribute, float amount)
+        //{
+        //    //attacker.GetComponentInChildren<Core>().GetCoreComponent<UnitStats>()
+        //    Debug.Log(core.transform.parent.name + " " + amount + " Damaged!");
+        //    stats.Comp?.DecreaseHealth(elementalPower, attiribute, amount);            
+        //    //if (damageParticles == null)
+        //    //{
+        //    //    Debug.Log("Combat DamageParticles is Null");
+        //    //    return;
+        //    //}
+        //    //particleManager.Comp?.StartParticlesWithRandomRotation(damageParticles);
+        //}
         public void Damage(StatsData AttackterCommonData, StatsData VictimCommonData, GameObject EffectPrefab, float amount)
         {
             Debug.Log(core.transform.parent.name + " " + amount + " Damaged!");
-            
+
             //TODO: 계산한 데미지 수치 return 필요
             //stats.Comp?.DecreaseHealth(AttackterCommonData, VictimCommonData, amount);
-            var damage =  stats.Comp.DecreaseHealth(AttackterCommonData, VictimCommonData, amount);
+            var damage = stats.Comp.DecreaseHealth(AttackterCommonData, VictimCommonData, amount);
             if (EffectPrefab == null)
             {
                 Debug.Log("Combat DamageParticles is Null");
                 return;
             }
 
-            //간결화 필요
-            var particle = particleManager.Comp?.StartParticlesWithRandomPosition(EffectPrefab, 1.5f);
+            RandomParticleInstantiate(EffectPrefab, 1.0f, damage, 50, AttackterCommonData.DamageAttiribute);
+        }
+
+        /// <summary>
+        /// Random위치에 파티클을 생성하고 UI상으로 같은 위치에 DamageText를 생성하는 로직
+        /// </summary>
+        /// <param name="effectPrefab">생성할 파티클</param>
+        /// <param name="range">피격위치로부터 랜덤 위치값을 가져올 구 범위의 반지름</param>
+        /// <param name="damage">피격 데미지</param>
+        /// <param name="fontSize">DamageText 폰트 사이즈</param>
+        /// <param name="damageAttiribute">Damage속성</param>
+        /// <returns></returns>
+        public GameObject RandomParticleInstantiate(GameObject effectPrefab, float range, float damage, float fontSize, DamageAttiribute damageAttiribute)
+        {
+            var particle = particleManager.Comp?.StartParticlesWithRandomPosition(effectPrefab, range);
 
             var pos = new Vector2((Camera.main.WorldToViewportPoint(particle.transform.position).x * GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.x) - (GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.x * 0.5f),
-                                    (Camera.main.WorldToViewportPoint(particle.transform.position).y * GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.y) - (GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.y * 0.5f) + 50);
+                                    (Camera.main.WorldToViewportPoint(particle.transform.position).y * GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.y) - (GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.y * 0.5f) + 50);   //50 = DamageText의 높이
             var damageText = Instantiate(GameManager.Inst.player.GetComponent<Player>().DamageTextPrefab,
                             new Vector3(pos.x, pos.y),
                             Quaternion.identity, GameManager.Inst.DamageUI.transform);
-            //간결화 필요
 
             damageText.GetComponent<RectTransform>().anchoredPosition = pos;
-            damageText.GetComponentInChildren<DamageText>().SetText(damage, 50, Color.blue);
+            switch (damageAttiribute)
+            {
+                case DamageAttiribute.Magic:
+                    damageText.GetComponentInChildren<DamageText>().SetText(damage, fontSize, Color.magenta);
+                    break;
+                case DamageAttiribute.Physics:
+                    damageText.GetComponentInChildren<DamageText>().SetText(damage, fontSize, Color.yellow);
+                    break;
+                case DamageAttiribute.Fixed:
+                    damageText.GetComponentInChildren<DamageText>().SetText(damage, fontSize, Color.black);
+                    break;
+            }
+
+            return damageText;
         }
 
         protected override void Awake()
