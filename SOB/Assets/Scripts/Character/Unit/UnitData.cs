9f3cfebd1f414f04d71b7aacf1a7c107f8582a03
KDH <ekgns0131@daum.net>
2023-02-16 19:41:34 +0900

    Fix UnitData  CreateAssetMenu

--- SOB/Assets/Scripts/Character/Unit/UnitData.cs
+++ SOB/Assets/Scripts/Character/Unit/UnitData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "newUnitData",menuName ="Data/Unit Data")]
 public class UnitData : ScriptableObject
 {
     [Header("Status")]
