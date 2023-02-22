70214ffb5dc9572a7c483efd42b8ca14dcdf759d
KDH <ekgns0131@daum.net>
2023-02-22 16:24:57 +0900

    Add Drop.PointerClick to InventoryDescript

--- SOB/Assets/Scripts/UI/Sub/InventoryDescript.cs
+++ SOB/Assets/Scripts/UI/Sub/InventoryDescript.cs
@@ -8,22 +8,35 @@ public class InventoryDescript : MonoBehaviour
 {
     public TextMeshProUGUI ItemName;
     public TextMeshProUGUI ItemDescript;
+    public TextMeshProUGUI DropButton;
 
     public void OnEnable()
     {
-        
+        SetDescript();
     }
 
     public void SetDescript()
     {
         var item = GameManager.Inst.SubUI.InventorySubUI.InventoryItems.CurrentSelectItem;
-        if (item != null && item.StatsItemData!=null)
+        if (item != null && item.StatsItemData != null)
         {
-                ItemName.text = item.StatsItemData.ItemName;
-                ItemDescript.text = item.StatsItemData.ItemDescription;
-                return;
+            ItemName.text = item.StatsItemData.ItemName;
+            ItemDescript.text = item.StatsItemData.ItemDescription;
+            DropButton.enabled = true;
+            return;
         }
         ItemName.text = "";
         ItemDescript.text = "";
+        DropButton.enabled = false;
+    }
+
+    public void Drop()
+    {
+        var item = GameManager.Inst.SubUI.InventorySubUI.InventoryItems.CurrentSelectItem;
+        if (item != null)
+        {
+            GameManager.Inst.player.Inventory.RemoveInventoryItem(item.StatsItemData);
+            SetDescript();
+        }
     }
 }
