cc363cd61ee8dbf2d7debb406cb0342f54e56d72
KDH <ekgns0131@daum.net>
2023-02-27 22:36:55 +0900

    Add RespawnPoint, Test EnemySpawn

--- SOB/Assets/Scripts/Manager/Component/SpawnManager.cs
+++ SOB/Assets/Scripts/Manager/Component/SpawnManager.cs
@@ -7,11 +7,52 @@ namespace SOB.Manager
 {
     public class SpawnManager : MonoBehaviour
     {
+        public GameObject TestEnemy;
+        private RespawnPoint[] respawnPoints;
+
+        private void OnEnable()
+        {
+            respawnPoints = GetComponentsInChildren<RespawnPoint>();
+
+            //Test
+            for(int i = 0; i < respawnPoints.Length; i++)
+            {
+                SpawnEnemy(TestEnemy);
+            }
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
         public void SpawnItem(GameObject SpawnPrefab, Vector3 pos,Transform transform, StatsItemSO itemData)
         {
             var item = Instantiate(SpawnPrefab, pos, Quaternion.identity, transform);
             item.GetComponent<SOB_Item>().Item = itemData;
             item.GetComponent<SOB_Item>().Init();
+        }
+
+        public void SpawnEnemy(GameObject EnemyPrefab, Vector3 pos, Transform transform)
+        {
+            var enemy = Instantiate(EnemyPrefab, pos, Quaternion.identity, transform);
+        }
+
+        public void SpawnEnemy(GameObject EnemyPrefab, Vector3 pos, Transform transform, EnemyData enemyData)
+        {
+            var enemy = Instantiate(EnemyPrefab, pos, Quaternion.identity, transform);
+            enemy.GetComponent<Enemy>().enemyData = enemyData;
+        }
+
+        public void SpawnEnemy(GameObject EnemyPrefab)
+        {
+            if(respawnPoints.Length <= 0)
+            {
+                Debug.LogWarning("RespawnPoints.Length < 0");
+                return;
+            }
+
+            var enemy = Instantiate(EnemyPrefab, respawnPoints[Random.Range(0, respawnPoints.Length)].transform.position, Quaternion.identity);            
         }
     }
 }
