1f15bd1728e6a0bb955299f78b5c51f51aaac579
KDH <ekgns0131@daum.net>
2023-03-16 23:44:41 +0900

    Add Title Layer

---
+++ SOB/Assets/Scripts/BackgroundMap/ConveyorMap.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class ConveyorMap : MonoBehaviour
+{
+    public SpriteRenderer[] Layers;
+
+    public float BetweenDistance;
+    public float ConveyorLine;
+    [Min(0.1f)]
+    public float Speed = 1f;
+    public bool isLeft = true;
+    private void Awake()
+    {
+        Layers = GetComponentsInChildren<SpriteRenderer>();
+        
+        for(int i =0; i <Layers.Length;i++)
+        {
+            if(isLeft)
+            {
+                Layers[i].gameObject.transform.Translate(Vector2.right * BetweenDistance * i);
+            }
+            else
+            {
+                Layers[i].gameObject.transform.Translate(Vector2.left * BetweenDistance * i);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Layers == null)
+            return;
+
+        for (int i = 0; i < Layers.Length; i++)
+        {
+            if(isLeft)
+            {
+                Layers[i].gameObject.transform.Translate(Vector2.left * 0.1f * Speed);
+                if (Layers[i].gameObject.transform.position.x < -ConveyorLine)
+                {
+                    Layers[i].gameObject.transform.Translate(Vector2.right * BetweenDistance * 2f);
+                }
+            }
+            else
+            {
+                Layers[i].gameObject.transform.Translate(Vector2.right * 0.1f * Speed);
+                if (Layers[i].gameObject.transform.position.x < -ConveyorLine)
+                {
+                    Layers[i].gameObject.transform.Translate(Vector2.left * BetweenDistance * 2f);
+                }
+            }
+        }
+    }
+}
+
