0f581ccb964663824569c312b59dd92522b513ee
KDH <ekgns0131@daum.net>
2023-03-28 00:52:44 +0900

    Fix ActionMovement 기존 Grounded와 InAir로 나뉘었던 부분을 ActionData단에서 구분하여 Command에 맞게 적용

--- SOB/Assets/Scripts/Action/Weapons/Components/ComponentsData/ActionData/ActionMovement.cs
+++ SOB/Assets/Scripts/Action/Weapons/Components/ComponentsData/ActionData/ActionMovement.cs
@@ -8,9 +8,16 @@ namespace SOB.Weapons.Components
     [Serializable]
     public class ActionMovement : ActionData
     {
-        [field: SerializeField] public bool CanFlip { get; private set; }
-        [field: SerializeField] public bool CanMoveCtrl { get; private set; }
-        [field: SerializeField] public Vector2 Direction { get; private set; }
-        [field: SerializeField] public float Velocity { get; private set; }
+        [field: SerializeField] public MovementCommandData[] movementCommands { get; private set; }
+
+        [Serializable]
+        public struct MovementCommandData
+        {
+            public bool CanFlip;
+            public bool CanMoveCtrl ;
+            public Vector2 Direction ;
+            public float Velocity ;
+            public CommandEnum Command ;
+        }    
     }
 }
--- SOB/Assets/Scripts/Action/Weapons/Components/WeaponMovement.cs
+++ SOB/Assets/Scripts/Action/Weapons/Components/WeaponMovement.cs
@@ -9,6 +9,12 @@ namespace SOB.Weapons.Components
 {
     public class WeaponMovement : WeaponComponent<MovementData, ActionMovement>
     {
+        private int currentMovementCommandIndex = 0;
+        protected override void HandleEnter()
+        {
+            base.HandleEnter();
+            currentMovementCommandIndex = 0;
+        }
         private void HandleStartMovement()
         {
             CoreMovement.CanMovement = true;
@@ -17,7 +23,26 @@ namespace SOB.Weapons.Components
         //중력의 영향 x 
         private void HandleFixedStartMovement()
         {
-            CoreMovement.SetVelocity(currentActionData.Velocity, currentActionData.Direction, CoreMovement.FancingDirection);
+            if (currentGroundedActionData != null && currentAirActionData != null)
+            {
+                if (weapon.InAir)
+                {
+                    CheckMovementAction(currentAirActionData);
+                }
+                else
+                {
+                    CheckMovementAction(currentGroundedActionData);
+                }
+            }
+            else if (currentGroundedActionData == null)
+            {
+                CheckMovementAction(currentAirActionData);
+            }
+            else if (currentAirActionData == null)
+            {
+                CheckMovementAction(currentGroundedActionData);
+            }
+            currentMovementCommandIndex++;
             core.Unit.RB.gravityScale = 0f;            
         }
         private void HandleFixedStopMovement()
@@ -40,6 +65,32 @@ namespace SOB.Weapons.Components
             CoreMovement.CanFlip = false;
         }
 
+        public void CheckMovementAction(ActionMovement actionData)
+        {
+            if (actionData == null)
+                return;
+            var currMovement = actionData.movementCommands;
+            if (currMovement.Length <= 0)
+                return;
+
+            for (int i = 0; i < currMovement.Length; i++)
+            {
+                if (currMovement[i].Command == weapon.Command)
+                {
+                    currentMovementCommandIndex = i;
+                    break;
+                }
+                currentMovementCommandIndex = -1;
+
+                if (currentMovementCommandIndex == -1)
+                {
+                    weapon.EventHandler.AnimationFinishedTrigger();
+                    return;
+                }
+            }
+
+            CoreMovement.SetVelocity(currMovement[currentMovementCommandIndex].Velocity, currMovement[currentMovementCommandIndex].Direction, CoreMovement.FancingDirection);
+        }
 
         protected override void Start()
         {
