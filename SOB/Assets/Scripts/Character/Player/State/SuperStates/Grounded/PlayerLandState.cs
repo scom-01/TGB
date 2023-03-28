44aad457e1d8256c145138d786faafe32a8ae6e1
KDH <ekgns0131@daum.net>
2023-03-28 20:06:48 +0900

    Add Landing Effect

--- SOB/Assets/Scripts/Character/Player/State/SuperStates/Grounded/PlayerLandState.cs
+++ SOB/Assets/Scripts/Character/Player/State/SuperStates/Grounded/PlayerLandState.cs
@@ -1,11 +1,23 @@
+using SOB.CoreSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLandState : PlayerGroundedState
 {
+    private GameObject Land_Effect;
     public PlayerLandState(Unit unit, string animBoolName) : base(unit, animBoolName)
     {
+        if (Land_Effect == null)
+        {
+            Land_Effect = Resources.Load<GameObject>("Prefabs/Particle/Landing_Smoke");
+        }
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        player.Core.GetCoreComponent<ParticleManager>().StartParticles(Land_Effect, CollisionSenses.GroundCheck.position);
     }
 
     public override void LogicUpdate()
