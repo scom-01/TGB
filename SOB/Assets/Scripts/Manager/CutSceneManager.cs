867ff8c88f3e724d04e3a71b17d51c4a6c8ad966
KDH <ekgns0131@daum.net>
2023-04-18 23:59:03 +0900

    Add SignalTrack, 컷씬에서 조절가능한 사운드 추가

--- SOB/Assets/Scripts/Manager/CutSceneManager.cs
+++ SOB/Assets/Scripts/Manager/CutSceneManager.cs
@@ -7,8 +7,54 @@ public class CutSceneManager : MonoBehaviour
 {
     [SerializeField] private PlayableDirector PlayableDirector;
 
-    public void OnTriggerCutSceneEnd()
+    public void OnTriggerSoundTrackStart(AudioClip audioClip)
     {
+        var sounds = GameObject.FindGameObjectsWithTag(GlobalValue.SoundContainerTagName);
+        if (sounds.Length > 0)
+        {
+            for (int i = 0; i < sounds.Length; i++)
+            {
+                if (sounds[i].GetComponent<SoundSync>() == null)
+                    continue;
 
+                if (sounds[i].GetComponent<SoundSync>().isBGM)
+                {
+                    var audioSource = sounds[i].AddComponent<AudioSource>();
+                    audioSource.clip = audioClip;
+                    audioSource.playOnAwake = true;
+                    audioSource.loop = false;
+                    if (DataManager.Inst != null)
+                    {
+                        audioSource.volume = DataManager.Inst.BGM_Volume;
+                    }
+                    else
+                    {
+                        audioSource.volume = 1;
+                    }
+                    audioSource.Play();
+                    Destroy(audioSource, audioSource.clip.length);
+                    break;
+                }
+                else
+                {
+                    var audioSource = sounds[i].AddComponent<AudioSource>();
+                    audioSource.clip = audioClip;
+                    audioSource.playOnAwake = true;
+                    audioSource.loop = false;
+                    if (DataManager.Inst != null)
+                    {
+                        audioSource.volume = DataManager.Inst.SFX_Volume;
+                    }
+                    else
+                    {
+                        audioSource.volume = 1;
+                    }
+                    audioSource.Play();
+                    Destroy(audioSource, audioSource.clip.length);
+                    break;
+                }
+            }
+        }
+        Debug.Log("Test CutScene");
     }
 }
