160c16c3e8ae53a558f753fe68842a659896e0f3
KDH <ekgns0131@daum.net>
2023-04-18 22:58:32 +0900

    Add CutSceneManager

---
+++ SOB/Assets/Scripts/Manager/CutSceneManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSceneManager : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector PlayableDirector;
+
+    public void OnTriggerCutSceneEnd()
+    {
+
+    }
+}
