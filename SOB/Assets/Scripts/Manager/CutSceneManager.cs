d00629281b416c67df7033e77011ba3232db8ffd
KDH <ekgns0131@daum.net>
2023-04-24 15:35:53 +0900

    Fix CutScene1 컷신 수정 및 씬 종료 추가

--- SOB/Assets/Scripts/Manager/CutSceneManager.cs
+++ SOB/Assets/Scripts/Manager/CutSceneManager.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class CutSceneManager : MonoBehaviour
 {
     [SerializeField] private PlayableDirector PlayableDirector;
+    [SerializeField] private string SceneName;
 
+    public void OnTriggerSceneEnd()
+    {
+        DataManager.Inst.NextStage(SceneName);
+        GameManager.Inst.ClearScene();
+    }
 }
