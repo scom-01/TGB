261d6769aefacd7ceafd1b3b8609e869d5fe4f9e
KDH <ekgns0131@daum.net>
2023-04-24 23:51:39 +0900

    Add BuffSystem BuffSystem에서 일괄적으로 Buff의 상태를 관리할 수 있도록 수정 코루틴으로 작동하지 않기에 Player값을 넘겨줄때 이전 추가된 Stats값이 사라지지않는 오류 수정

--- SOB/Assets/Scripts/Item/Buff/Buff.cs
+++ SOB/Assets/Scripts/Item/Buff/Buff.cs
@@ -1,9 +1,10 @@
 using SOB.CoreSystem;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[Serializable]
 public class Buff 
 {
     public StatsData statsData;
--- SOB/Assets/Scripts/Item/Buff/BuffSystem.cs
+++ SOB/Assets/Scripts/Item/Buff/BuffSystem.cs
@@ -1,4 +1,5 @@
 using SOB.CoreSystem;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,13 +24,52 @@ public class BuffSystem : MonoBehaviour
     {
 
     }
+    private void Update()
+    {
+        if (buffs.Count <= 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < buffs.Count;)
+        {
+            
+            if (Time.time >= buffs[i].startTime + buffs[i].durationTime)
+            {
+                unit.Core.GetCoreComponent<UnitStats>().StatsData += buffs[i].statsData * -1f;
+                if (unit.Core.GetCoreComponent<UnitStats>().CurrentHealth > buffs[i].statsData.MaxHealth)
+                {
+                    unit.Core.GetCoreComponent<UnitStats>().CurrentHealth += buffs[i].statsData.MaxHealth * -1f;
+                }
+                buffs.RemoveAt(i);
+                if (buffs.Count <= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                Debug.Log($"{Time.time} <= {buffs[i].startTime + buffs[i].durationTime}");
+            }
+            i++;
+        }
+    }
 
     public void AddBuff(Buff buff)
     {
         buff.startTime = Time.time;
         buffs?.Add(buff);
         GameManager.Inst?.MainUI?.MainPanel?.BuffPanelSystem.BuffPanelAdd(buff);
-        PlayBuff(buff);
+        unit.Core.GetCoreComponent<UnitStats>().StatsData += buff.statsData;
+        if (buff.statsData.MaxHealth != 0.0f)
+        {
+            unit.Core.GetCoreComponent<UnitStats>().CurrentHealth += buff.statsData.MaxHealth;
+        }
+        //PlayBuff(buff);
     }
 
     public void RemoveBuff(Buff buff)
