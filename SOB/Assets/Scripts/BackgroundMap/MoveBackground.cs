4a3dbfcaa3042037075153c5eca5e029709ccf6a
KDH <ekgns0131@daum.net>
2023-05-04 20:45:11 +0900

    Add MoveBackground Camera.main을 따라다니는 Background Sprite

---
+++ SOB/Assets/Scripts/BackgroundMap/MoveBackground.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveBackground : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private float MoveSpeed;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cam == null)
+            return;
+
+        transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, 0);
+    }
+}
