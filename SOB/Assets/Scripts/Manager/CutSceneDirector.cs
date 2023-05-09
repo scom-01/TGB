19feb0d3f9ea5958dc38430a45cc5fde1770ef87
KDH <ekgns0131@daum.net>
2023-05-09 23:44:06 +0900

    Fix CutScene 컷신 디테일 수정

---
+++ SOB/Assets/Scripts/Manager/CutSceneDirector.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSceneDirector : MonoBehaviour
+{
+    public List<PlayableDirector> playableDirectors = new List<PlayableDirector>();
+
+}
