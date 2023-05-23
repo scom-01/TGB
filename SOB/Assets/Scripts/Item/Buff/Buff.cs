52382c33a020860cb6bd3bdbbb599615e46ff3f7
KDH <ekgns0131@daum.net>
2023-05-23 19:26:42 +0900

    Fix Buff Buff안에 더 많은 내용을 저장하여 사용하도록 수정

--- SOB/Assets/Scripts/Item/Buff/Buff.cs
+++ SOB/Assets/Scripts/Item/Buff/Buff.cs
@@ -7,8 +7,12 @@ using UnityEngine;
 [Serializable]
 public class Buff 
 {
-    public BuffItemSO buffItem;
+    //public BuffItemSO buffItem;
+    public BuffItem_Data buffItem;
+    public StatsData statsData;
+    public EffectData effectData;
+    public ItemData itemData;
     public int CurrBuffCount = 0;
-
+    public List<ItemEffectSO> itemEffects = new List<ItemEffectSO>();
     public float startTime;
 }
