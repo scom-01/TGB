48b7ca56c272954a56ed943f4154ba760ee31e32
KDH <ekgns0131@daum.net>
2023-05-25 23:17:45 +0900

    Add Stage1 Boss script

---
+++ SOB/Assets/Scripts/Character/Enemy/Boss/State/Boss_Static_Stage_1_AttackState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Static_Stage_1_AttackState : EnemyAttackState
+{
+    private Boss_Static_Stage_1 boss_Static_Stage_1;
+
+    public Boss_Static_Stage_1_AttackState(Unit unit, string animBoolName) : base(unit, animBoolName)
+    {
+        boss_Static_Stage_1 = enemy as Boss_Static_Stage_1;
+    }
+    public override void IdleState()
+    {
+        enemy.FSM.ChangeState(boss_Static_Stage_1.RunState);
+    }
+}
