b9713ab82f2232b8bafb36bc2040ec0a5d9cc3e7
KDH <ekgns0131@daum.net>
2023-05-29 21:55:52 +0900

    Fix GameManager. InventoryItem Fix Put InventoryItem

--- SOB/Assets/Scripts/Manager/GameManager.cs
+++ SOB/Assets/Scripts/Manager/GameManager.cs
@@ -217,6 +217,7 @@ public class GameManager : MonoBehaviour
                     MainUI.Canvas.enabled = false;
                 break;
             case UI_State.Inventory:
+                SubUI.InventorySubUI.PutInventoryItem();
                 SubUI.InventorySubUI.Canvas.enabled = true;
                 if (SubUI.InventorySubUI.InventoryItems.CurrentSelectItem == null)
                 {
