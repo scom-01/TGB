28427b951ff1d2aee9a5f4126eb715743fad6359
KDH <ekgns0131@daum.net>
2023-06-15 20:38:35 +0900

    Fix QualityLevelSetting 텍스트 변환 후 LocalizationStringEvent 수정으로 오류 수정

--- SOB/Assets/Scripts/UI/Config/Setting/QualityLevelSetting.cs
+++ SOB/Assets/Scripts/UI/Config/Setting/QualityLevelSetting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
 using UnityEngine.Localization.Settings;
 
 namespace SOB.CfgSetting
@@ -10,23 +11,14 @@ namespace SOB.CfgSetting
     public class QualityLevelSetting : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI LevelTxt;
-
         private void OnEnable()
         {
-            DataManager.Inst?.PlayerCfgQualityLoad();
-            ChangeText();
+            DataManager.Inst?.PlayerCfgQualityLoad();            
         }
 
-        private void Start()
-        {
-            LocalizationSettings.SelectedLocaleChanged += ChangeText;
-        }
-
-        private void OnDestroy()
-        {            
-            LocalizationSettings.SelectedLocaleChanged -= ChangeText;
-        }
-
+        /// <summary>
+        /// Quality Left Click
+        /// </summary>
         public void OnClickLeft()
         {
             if (QualitySettings.GetQualityLevel() == 0)
@@ -40,6 +32,9 @@ namespace SOB.CfgSetting
             ChangeText();
             DataManager.Inst?.PlayerCfgQualitySave();
         }
+        /// <summary>
+        /// Quality Right Click
+        /// </summary>
         public void OnClickRight()
         {
             if (QualitySettings.GetQualityLevel() == QualitySettings.names.Length - 1)
@@ -54,19 +49,20 @@ namespace SOB.CfgSetting
             DataManager.Inst?.PlayerCfgQualitySave();
         }
 
-        private void ChangeText()
+        public void ChangeText()
         {
-            Locale currentLocale = LocalizationSettings.SelectedLocale;
-            var str = Resources.Load(GlobalValue.UI_Table).name;
-            //Quality의 설정값과 동일한 Key값을 미리 UI Table안에 설정해놓음 ex)LOW : 낮음            
-            LevelTxt.text = LocalizationSettings.StringDatabase.GetLocalizedString(str, (QualitySettings.names[QualitySettings.GetQualityLevel()]).ToString().ToUpper(), currentLocale);
-        }
-        private void ChangeText(Locale locale)
-        {
-            Locale currentLocale = LocalizationSettings.SelectedLocale;
-            var str = Resources.Load(GlobalValue.UI_Table).name;
-            //Quality의 설정값과 동일한 Key값을 미리 UI Table안에 설정해놓음 ex)LOW : 낮음);
-            LevelTxt.text = LocalizationSettings.StringDatabase.GetLocalizedString(str, (QualitySettings.names[QualitySettings.GetQualityLevel()]).ToString().ToUpper(), currentLocale);            
+            //Quality의 설정값과 동일한 Key값을 미리 UI Table안에 설정해놓음 ex)LOW : 낮음
+            //var str = Resources.Load(GlobalValue.UI_Table).name;
+            LevelTxt.text = (QualitySettings.names[QualitySettings.GetQualityLevel()]).ToString().ToUpper();
+            if (DataManager.Inst != null)
+            {
+                var table = DataManager.Inst?.localizationSettings.GetStringDatabase().DefaultTable ?? "UI_Table";
+                LevelTxt.GetComponent<LocalizeStringEvent>()?.StringReference.SetReference(table, LevelTxt.text);
+            }   
+            else
+            {
+                LevelTxt.GetComponent<LocalizeStringEvent>()?.StringReference.SetReference("UI_Table", LevelTxt.text);
+            }
         }
     }
 }
