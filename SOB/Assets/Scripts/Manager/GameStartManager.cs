ed7a19ed1743697e382f83204b7126dbec42b3d1
KDH <ekgns0131@daum.net>
2023-06-15 21:16:43 +0900

    Add GameStartManager LocalizationSettings.InitializationOperation.IsDone 체크

--- SOB/Assets/Scripts/Manager/GameStartManager.cs
+++ SOB/Assets/Scripts/Manager/GameStartManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 
 public class GameStartManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool isDone = false;
+    private void Awake()
     {
         DataManager.Inst?.NextStage("Title");
-        AsyncOperation operation = SceneManager.LoadSceneAsync("LoadingScene");
+    }
+
+    private void Update()
+    {
+        if (isDone)
+            return;
+
+        if(LocalizationSettings.InitializationOperation.IsDone)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync("LoadingScene");
+            isDone = true;
+        }
     }
 }
