fdfc8d6ac949f2556654cc2fe91387e516a62d46
KDH <ekgns0131@daum.net>
2023-06-15 20:39:54 +0900

    Fix ConveyorMap 퀄리티 세팅 변경에 따른 프레임 변화에도 속도가 같도록 FixedUpdate로 수정

--- SOB/Assets/Scripts/BackgroundMap/ConveyorMap.cs
+++ SOB/Assets/Scripts/BackgroundMap/ConveyorMap.cs
@@ -29,7 +29,7 @@ public class ConveyorMap : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Layers == null)
             return;
