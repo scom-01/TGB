41abf60d2f7708cce19b2ebe0c1f535aaacc20dc
KDH <ekgns0131@daum.net>
2023-06-17 20:57:06 +0900

    Fix OpenGate Add isDone

--- SOB/Assets/Scripts/Interactive/OpenGate.cs
+++ SOB/Assets/Scripts/Interactive/OpenGate.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class OpenGate : InteractiveObject
 {
+    private bool isDone = false;
+
+    private void Awake()
+    {
+        isDone = false;
+    }
     public override void Interactive()
     {
+        if (isDone)
+            return;
+
+        Debug.LogWarning("OpenGate");
         GameManager.Inst.inputHandler.ChangeCurrentActionMap(InputEnum.Cfg, false);
         GameManager.Inst?.ClearScene();
+        isDone = true;
     }
 }
