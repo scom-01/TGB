e042b714161225bb12970146c1257f34d93c3221
KDH <ekgns0131@daum.net>
2023-06-30 18:55:33 +0900

    Fix HealthBar Player HealthBar

--- SOB/Assets/Scripts/Character/Enemy/HealthBar.cs
+++ SOB/Assets/Scripts/Character/Enemy/HealthBar.cs
@@ -1,33 +1,91 @@
 using SOB.CoreSystem;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    private Unit Unit
+    {
+        get
+        {
+            if (unit == null)
+            {
+                if (isSpriteRender)
+                {
+                    unit = this.GetComponentInParent<Unit>();                    
+                }
+                else
+                {
+                    if (GameManager.Inst?.StageManager != null)
+                    {
+                        unit = GameManager.Inst.StageManager.player;
+                    }
+                }
+            }
+            return unit;
+        }
+    }
     private Unit unit;
+    private UnitStats Stats
+    {
+        get
+        {
+            if (stats == null)
+            {
+                if (Unit == null)
+                {
+                    return null;
+                }
+                stats = Unit.Core.GetCoreComponent<UnitStats>();
+            }
+            return stats;
+        }
+    }
     private UnitStats stats;
+
+    [SerializeField] private bool isSpriteRender = true;
     [SerializeField] private RectTransform healthbarTrasform;
     [SerializeField] private float ZeroPosX;
-    // Start is called before the first frame update
-    void Start()
-    {
-        unit = GetComponentInParent<Unit>();
-        stats = unit.Core.GetCoreComponent<UnitStats>();
-    }
+
+    private Image Img;
+    private TextMeshProUGUI Txt;
 
     // Update is called once per frame
     void Update()
     {
-        if (stats == null)
+        if (GameManager.Inst?.StageManager == null)
         {
-            if (unit == null)
-            {
-                unit = GetComponentInParent<Unit>();
-            }
-            stats = unit.Core.GetCoreComponent<UnitStats>();
+            return;
         }
-        healthbarTrasform.anchoredPosition = new Vector2(ZeroPosX * (1 - (stats.CurrentHealth / stats.StatsData.MaxHealth)), 0);
+
+        if (Stats == null)
+        {
+
+            return;
+        }
+
+        if (isSpriteRender)
+        {
+            healthbarTrasform.anchoredPosition = new Vector2(ZeroPosX * (1 - (Stats.CurrentHealth / Stats.StatsData.MaxHealth)), 0);
+        }
+        else
+        {
+            Img = this.GetComponent<Image>();
+            Txt = this.GetComponentInChildren<TextMeshProUGUI>();
+            Img.fillAmount = Stats.CurrentHealth / Stats.StatsData.MaxHealth;
+            Txt.text = string.Format($"{Stats.CurrentHealth} / {Stats.StatsData.MaxHealth}");
+            if (Img.fillAmount < 0.3f)
+            {
+                healthbarTrasform.GetComponent<OverlayImg>().SetValue(1 - (Img.fillAmount / 0.3f));
+            }
+            else
+            {
+                healthbarTrasform.GetComponent<OverlayImg>().SetValue(0);
+            }
+        }
     }
 }
