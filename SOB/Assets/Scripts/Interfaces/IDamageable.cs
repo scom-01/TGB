f8e68f40f00ad4e32f58ed827e2026e9421482a5
KDH <ekgns0131@daum.net>
2023-07-25 14:05:51 +0900

    Fix IDamagable

--- SOB/Assets/Scripts/Interfaces/IDamageable.cs
+++ SOB/Assets/Scripts/Interfaces/IDamageable.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public interface IDamageable
 {
     public void Damage(StatsData AttackterCommonData, StatsData VictimCommonData, float amount);
+    public void Damage(StatsData AttackterCommonData, StatsData VictimCommonData, E_Power _elemental, float amount);
     public void Damage(StatsData AttackterCommonData, StatsData VictimCommonData, float amount, int repeat);
+    public void FixedDamage(int amount);
     public void HitEffect(GameObject EffectPrefab, float Range, int FancingDirection);
 }
