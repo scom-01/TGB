e9b2138d4d2ae7f4381ae8ffefd37fb859b11e30
KDH <ekgns0131@daum.net>
2023-08-07 01:41:48 +0900

    Fix HealthBar HP바 슬라이더 최적화 및 HP바 뒤에 이전HP를 간접적으로 보여주는 BackHealthBarEffect 추가

--- SOB/Assets/Scripts/Character/HealthBar.cs
+++ SOB/Assets/Scripts/Character/HealthBar.cs
@@ -49,11 +49,24 @@ public class HealthBar : MonoBehaviour
     }
     private UnitStats stats;
     [SerializeField] private Slider m_Slider;
-    [SerializeField] private BackHealthBarEffect HealthBarEffect;
     [SerializeField] private bool m_isUnit = true;
     [SerializeField] private bool m_IsFollow = true;
     private Image Img;
     [SerializeField] private TextMeshProUGUI Txt;
+    [SerializeField] private BackHealthBarEffect HealthBarEffect
+    {
+        get
+        {
+            if(m_HealthBarEffect == null)
+            {
+                m_HealthBarEffect = this.GetComponentInChildren<BackHealthBarEffect>();
+            }
+            return m_HealthBarEffect;
+        }
+    }
+    private BackHealthBarEffect m_HealthBarEffect;
+    [SerializeField] private float lerpduration = 0.5f;
+    private Coroutine runningCoroutine;
 
     private void OnEnable()
     {
@@ -70,39 +83,59 @@ public class HealthBar : MonoBehaviour
             Stats.OnChangeHealth -= UpdateBar;
         }
     }
+
     private void UpdateBar()
     {
-        StartCoroutine(LerpHealthBar());
+        // 슬라이더의 목표 값을 계산합니다.
+        float targetValue = Stats.CurrentHealth / Stats.MaxHealth;
+
+
+        if (runningCoroutine != null) 
+        {
+            StopCoroutine(runningCoroutine);
+        }
+        runningCoroutine = StartCoroutine(LerpHealthBar(targetValue));
         if (Txt != null)
         {
             Txt.text = string.Format($"{(int)Stats.CurrentHealth} / {Stats.MaxHealth}");
-            Txt = this.GetComponentInChildren<TextMeshProUGUI>();
-        }
+        }                
     }
-
-    IEnumerator LerpHealthBar()
+    IEnumerator LerpHealthBar(float targetValue)
     {
         if (m_Slider != null)
         {
-            while(m_Slider.value > Stats.CurrentHealth / Stats.MaxHealth)
+            float lerpValue = m_Slider.value;
+            float startTime = Time.time;
+
+            while (lerpValue > targetValue)
             {
-                m_Slider.value = Mathf.Lerp(m_Slider.value, Stats.CurrentHealth / Stats.MaxHealth, Time.deltaTime * 5f);
+                float elapsedTime = Time.time - startTime;
+                lerpValue = Mathf.Lerp(m_Slider.value, targetValue, elapsedTime / lerpduration);
+                m_Slider.value = lerpValue;
 
                 if (m_IsFollow)
                 {
+                    Camera mainCamera = Camera.main; // 필요한 경우에만 Camera 변수를 사용합니다.
                     if (GameManager.Inst.StageManager.Cam != null)
                     {
-                        m_Slider.transform.position = GameManager.Inst.StageManager.Cam.WorldToScreenPoint(unit.Core.CoreCollisionSenses.GroundCenterPos + new Vector3(0.0f, -0.5f, 0.0f));
+                        mainCamera = GameManager.Inst.StageManager.Cam;
                     }
-                    else
-                    {
-                        m_Slider.transform.position = Camera.main.WorldToScreenPoint(unit.Core.CoreCollisionSenses.GroundCenterPos + new Vector3(0.0f, -0.5f, 0.0f));
-                    }
+
+                    m_Slider.transform.position = mainCamera.WorldToScreenPoint(unit.Core.CoreCollisionSenses.GroundCenterPos + new Vector3(0.0f, -0.5f, 0.0f));
                 }
+
                 yield return null;
             }
-            StartCoroutine(HealthBarEffect.Update_HealthEffect(m_Slider.value));
+
+            CallBackHealthBarEffect();
         }
+    }
+
+    void CallBackHealthBarEffect()
+    {
+        Debug.Log("Success UpdateHealthBar");
+        if (HealthBarEffect != null)
+            HealthBarEffect.Health_value = m_Slider.value;
     }
 
     private Canvas m_Canvas
