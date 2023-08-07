113ccf32a5c57c68e1b5cd5a79bd305b0152e2bd
KDH <ekgns0131@daum.net>
2023-08-08 01:16:04 +0900

    Fix MoveBackground 뒷배경 parallax infinite scrolling 구현

--- SOB/Assets/Scripts/BackgroundMap/MoveBackground.cs
+++ SOB/Assets/Scripts/BackgroundMap/MoveBackground.cs
@@ -1,24 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class MoveBackground : MonoBehaviour
 {
-    private Camera cam;
-    [SerializeField] private float MoveSpeed;
+    /// <summary>
+    /// 거리감
+    /// </summary>
+    [SerializeField] private Vector2 Distance;
+
+    [SerializeField] private bool infiniteHorizontal;
+    [SerializeField] private bool infiniteVertical;
+    private Transform camTransform;
+    private Vector3 lastCamPos;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
 
     private void Start()
     {
-        cam = Camera.main;
+        camTransform = Camera.main.transform;
+        if (GameManager.Inst.StageManager.Cam != null)
+        {
+            camTransform = GameManager.Inst.StageManager.Cam.transform;
+        }
+        lastCamPos = camTransform.position;
+        if(this.GetComponent<SpriteRenderer>().drawMode != SpriteDrawMode.Tiled)
+        {
+            this.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Tiled;
+            this.GetComponent<SpriteRenderer>().size = new Vector2(this.GetComponent<SpriteRenderer>().size.x * 3, this.GetComponent<SpriteRenderer>().size.y);
+        }
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;        
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width * transform.localScale.x / sprite.pixelsPerUnit;
+        textureUnitSizeY = texture.height * transform.localScale.y / sprite.pixelsPerUnit;
     }
 
+    [ExecuteInEditMode]
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (cam == null)
+        if (camTransform == null)
             return;
 
-        transform.position = new Vector3(cam.transform.position.x* MoveSpeed, cam.transform.position.y* MoveSpeed, 0);
+        Vector3 deltaMovement = camTransform.position - lastCamPos;
+        transform.position += new Vector3(deltaMovement.x * Distance.x, deltaMovement.y * Distance.y, 0);
+        lastCamPos = camTransform.position;
+
+        if (infiniteHorizontal)
+        {
+            if (Mathf.Abs(camTransform.position.x - transform.position.x) >= textureUnitSizeX)
+            {
+                float offsetPosX = (camTransform.position.x - transform.position.x) % textureUnitSizeX;
+                transform.position = new Vector3(camTransform.position.x + offsetPosX, transform.position.y);
+            }
+        }
+        if (infiniteVertical)
+        {
+            if (Mathf.Abs(camTransform.position.y - transform.position.y) >= textureUnitSizeY)
+            {
+                float offsetPosY = (camTransform.position.y - transform.position.y) % textureUnitSizeY;
+                transform.position = new Vector3(transform.position.x, offsetPosY + camTransform.position.y);
+            }
+        }
     }
 }
