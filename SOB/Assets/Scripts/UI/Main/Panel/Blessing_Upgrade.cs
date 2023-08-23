99372ad3e4a1f0560ae37e487257860bd28bbd60
KDH <ekgns0131@daum.net>
2023-08-24 00:58:53 +0900

    Fix Blessing_Upgrade 축복 체크 및 Text Update함수 보충

--- SOB/Assets/Scripts/UI/Main/Panel/Blessing_Upgrade.cs
+++ SOB/Assets/Scripts/UI/Main/Panel/Blessing_Upgrade.cs
@@ -14,30 +14,60 @@ public class Blessing_Upgrade : MonoBehaviour
     [SerializeField] private LocalizeStatsDataUpdater LocalizeStatsDataUpdater;
 
     [SerializeField] private TMP_Text TMP_BlessingCost;
-    [SerializeField] private LocalizeStringEvent LocalizeStringEvent__BlessingCost;
-
+    [SerializeField] private LocalizeStringEvent LocalizeStringEvent_BlessingCost;
+    [SerializeField] private int Max_level;
+    private int piece;
     //OnClick
     public void Set(Stats _stats)
     {
         Stats = _stats;
+
+        if (Stats.TypeSprite != null)
+        {
+            Icon.sprite = Stats.TypeSprite;
+        }
         LocalizeStringEvent_Descript.StringReference.SetReference("Stats_Table", Stats.TypeStr + "_Descript");
         LocalizeStatsDataUpdater.SetStats(_stats);
-
+        LocalizeStatsDataUpdater.UpdateLocalizedText();
+        Set_Blessing();
     }
 
     private void Set_Blessing()
     {
-        var piece = DataManager.Inst.JSON_DataParsing.m_JSON_DefaultData.hammer_piece;
+        piece = DataManager.Inst.JSON_DataParsing.m_JSON_DefaultData.hammer_piece;
 
         // 텍스트 업데이트
-        string localizedText = LocalizeStringEvent__BlessingCost.StringReference.GetLocalizedString();
-        localizedText += string.Format($" : <color={(piece < 200 * Stats.variable ? "red" : "green")}>{piece}</color> : {200 * Stats.variable}");
+        string localizedText = LocalizeStringEvent_BlessingCost.StringReference.GetLocalizedString();
+        localizedText += string.Format($" : <color={(CheckUpgrade(piece) ? "green" : "red")}>{piece}</color> : {200 * Stats.variable}");
 
         if (TMP_BlessingCost != null)
             TMP_BlessingCost.text = localizedText;
     }
     public void Upgrade()
     {
+        if (!CheckUpgrade(piece))
+        {
+            return;
+        }
+        DataManager.Inst.JSON_DataParsing.m_JSON_DefaultData.hammer_piece -= 200 * (int)Stats.variable;
+        if (DataManager.Inst.JSON_DataParsing.m_JSON_DefaultData.hammer_piece < 0)
+        {
+            DataManager.Inst.JSON_DataParsing.m_JSON_DefaultData.hammer_piece = 0;
+        }
+        LocalizeStatsDataUpdater.UpdateLocalizedText();
+    }
 
+    /// <summary>
+    /// true = enough goods
+    /// </summary>
+    /// <param name="_piece"></param>
+    /// <returns></returns>
+    private bool CheckUpgrade(int _piece)
+    {
+        if (Stats.variable >= Max_level)
+        {
+            return false;
+        }
+        return _piece >= 200 * Stats.variable;
     }
 }
