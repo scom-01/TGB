c9a91a9b9b6fceb4dbe152d7c3f568d82f95ce50
KDH <ekgns0131@daum.net>
2023-08-29 03:59:43 +0900

    Add CostText item의 상점 가격 설정

---
+++ SOB/Assets/Scripts/UI/Main/Panel/CostText.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
+using UnityEngine;
+
+
+public class CostText : MonoBehaviour
+{
+    [SerializeField] private InventoryItem item;
+    private TMP_Text Txt;
+    [SerializeField] private GOODS_TPYE goods_tpye;
+    private int cost = 0;
+    private string old_cost = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Txt = this.GetComponent<TMP_Text>();
+        Txt.text = "";
+    }
+
+    private void Update()
+    {
+        if (item?.StatsItemData == null)
+        {
+            Txt.text = "";
+            return;
+        }
+
+        UpdateCost();
+    }
+
+    private void UpdateCost()
+    {
+        Debug.Log("Enum = " + item.StatsItemData.itemData.ItemLevel);
+        Debug.Log("Enum(int) = " + (int)item.StatsItemData.itemData.ItemLevel);
+        cost = (int)item.StatsItemData.itemData.ItemLevel * GameManager.Inst.StageManager.StageLevel * GlobalValue.Gold_Inflation;
+
+        Txt.text = cost.ToString();
+        switch (goods_tpye)
+        {
+            case GOODS_TPYE.Gold:
+                Txt.text += "<color=yellow>G</color>";
+                break;
+            case GOODS_TPYE.FireGoods:
+                Txt.text += "<color=red>G</color>";
+                break;
+            case GOODS_TPYE.WaterGoods:
+                Txt.text += "<color=blue>W</color>";
+                break;
+            case GOODS_TPYE.EarthGoods:
+                Txt.text += "<color=grey>E</color>";
+                break;
+            case GOODS_TPYE.WindGoods:
+                Txt.text += "<color=white>Wind</color>";
+                break;
+        }
+        old_cost = Txt.text;
+    }
+}
