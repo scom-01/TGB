5b18951b9a21337e4596ff0d3d81e71bca573c17
KDH <ekgns0131@daum.net>
2023-11-10 03:02:02 +0900

    Fix OpenGate 새 게임 시 SaveData()전에는 랜덤변수가 설정되지 않는 버그 수정

--- TGB/Assets/Scripts/Interactive/OpenGate.cs
+++ TGB/Assets/Scripts/Interactive/OpenGate.cs
@@ -18,13 +18,6 @@ public class OpenGate : InteractiveObject
         if (isDone)
             return;
 
-        //씬 이동할 때마다 게임 Idx 설정 //임의의 수 10
-        DataManager.Inst.JSON_DataParsing.m_JSON_SceneData.SceneDataIdxs.Clear();
-        for (int i = 0; i < 10; i++)
-        {
-            DataManager.Inst.JSON_DataParsing.m_JSON_SceneData.SceneDataIdxs.Add(UnityEngine.Random.Range(0, GlobalValue.MaxSceneIdx));
-        }
-
         Debug.LogWarning("OpenGate");
         GameManager.Inst.InputHandler.ChangeCurrentActionMap(InputEnum.Cfg, false);
         GameManager.Inst?.ClearScene();
--- TGB/Assets/Scripts/Manager/LoadSceneController.cs
+++ TGB/Assets/Scripts/Manager/LoadSceneController.cs
@@ -13,8 +13,16 @@ public class LoadSceneController : MonoBehaviour
 
         if (GameManager.Inst == null)
             return;
-                
-        GameManager.Inst.ChangeUI(UI_State.Loading);        
+
+        //씬 이동할 때마다 게임 Idx 설정 //임의의 수 10
+        DataManager.Inst.JSON_DataParsing.m_JSON_SceneData.SceneDataIdxs.Clear();
+        for (int i = 0; i < 10; i++)
+        {
+            DataManager.Inst.JSON_DataParsing.m_JSON_SceneData.SceneDataIdxs.Add(UnityEngine.Random.Range(0, GlobalValue.MaxSceneIdx));
+        }
+
+        GameManager.Inst.ChangeUI(UI_State.Loading);
+        
         GameManager.Inst.SaveData();
         AsyncOperation operation = SceneManager.LoadSceneAsync("LoadingScene");
         isMoveScene = true;
