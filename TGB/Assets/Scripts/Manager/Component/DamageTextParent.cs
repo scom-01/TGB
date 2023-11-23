0e5eb11b58c792dbe16665e59a9e2db0d6a7ef48
KDH <ekgns0131@daum.net>
2023-11-24 03:20:19 +0900

    Fix DamageTextParent DamageText가 생성된 위치에서 벗어나지 않는 것처럼 보이도록 수정

--- TGB/Assets/Scripts/Manager/Component/DamageTextParent.cs
+++ TGB/Assets/Scripts/Manager/Component/DamageTextParent.cs
@@ -1,10 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 [RequireComponent(typeof(RectTransform))]
 public class DamageTextParent : MonoBehaviour
 {
-    
+    private bool isOn =false;
+    private Vector3 lastCamPos = new Vector3();
+    private RectTransform RT
+    {
+        get
+        {
+            if(rectTransform==null)
+            {
+                rectTransform = this.GetComponent<RectTransform>();
+            }
+            return rectTransform;
+        }
+        set
+        {
+            rectTransform = value;
+        }
+    }
+    private RectTransform rectTransform = null;
+    private Camera Cam
+    {
+        get
+        {
+            if(cam ==null)
+            {
+                cam = Camera.main;
+            }
+            return cam;
+        }
+    }
+    private Camera cam = null;
+    private void OnEnable()
+    {
+        isOn = true;
+        lastCamPos = Cam.transform.position;
+    }
+    private void LateUpdate()
+    {
+        if (!isOn)
+            return;
+        Vector3 deltaMovement = Cam.transform.position - lastCamPos;
+
+        //현재 위치를 WorldSpace 위치로 전환
+        var ScreenPos = Cam.ScreenToWorldPoint(RT.position);
+
+        //전환한 위치를 카메라가 이동한만큼 빼주어 카메라가 볼 때 제자리에 있는 것 처럼
+        ScreenPos -= deltaMovement;
+        //ScreenPos를 viewport로 전환
+        var _pos = new Vector2(
+            (Cam.WorldToViewportPoint(ScreenPos).x * GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.x) - (GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.x * 0.5f),
+            (Cam.WorldToViewportPoint(ScreenPos).y * GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.y) - (GameManager.Inst.DamageUI.GetComponent<RectTransform>().sizeDelta.y * 0.5f)
+                            );
+        //전환한 Viewport 좌료를 RectTransform에 연결
+        RT.anchoredPosition = _pos;
+
+        lastCamPos = Cam.transform.position;
+    }
+
+    private void OnDisable()
+    {
+        isOn = false;
+    }
 }
