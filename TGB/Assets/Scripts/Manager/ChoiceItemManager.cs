6ccb346ea96cfc6f1560d81b5f9a12315e4d704a
KDH <ekgns0131@daum.net>
2023-12-13 14:27:43 +0900

    Fix ChoiceItemManager ItemSpawn 수정 기존 - 모든 적 처치 시 아이템 스폰 수정 - 모든 적 처치 후 BoxCollider2D에 OnTriggerEnter2D 시 스폰

--- TGB/Assets/Scripts/Manager/ChoiceItemManager.cs
+++ TGB/Assets/Scripts/Manager/ChoiceItemManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 
 public class ChoiceItemManager : MonoBehaviour
@@ -10,9 +11,29 @@ public class ChoiceItemManager : MonoBehaviour
     public int ChoiceItemAmount;
     private int CurrChoiceItemAmount = 0;
 
+    public float Iteminterval = 0f;
+
+    public bool isTriggerOn
+    {
+        get
+        {
+            return _isTriggerOn;
+        }
+        set
+        {
+            _isTriggerOn = value;
+            DetectedArea.enabled = value;
+        }
+    }
+    private bool _isTriggerOn = false;
+    private BoxCollider2D DetectedArea;
+
     private void Awake()
     {
         CurrChoiceItemAmount = 0;
+        DetectedArea = this.GetComponent<BoxCollider2D>();
+        DetectedArea.isTrigger = true;
+        DetectedArea.enabled = false;
     }
     // Update is called once per frame
     void Update()
@@ -27,11 +48,36 @@ public class ChoiceItemManager : MonoBehaviour
                 ItemList.RemoveAt(i);
                 CurrChoiceItemAmount++;
             }
-            if(CurrChoiceItemAmount >= ChoiceItemAmount)
+            if (CurrChoiceItemAmount >= ChoiceItemAmount)
             {
                 DestroyChoiceItemList();
                 break;
             }
+        }
+    }
+
+    public bool AddSpawnItem(GameObject obj, Vector3 pos, Transform transform, StatsItemSO itemData)
+    {
+        if (obj = null)
+            return false;
+
+        var item = GameManager.Inst.StageManager.IM.SpawnItem(obj, pos, transform, itemData);
+        if (item == null)
+        {
+            return false;
+        }
+
+        item.SetActive(false);
+        ItemList.Add(item);
+
+        return true;
+    }
+
+    private void ItemSpawn()
+    {
+        for (int i = 0; i < ItemList.Count; i++)
+        {
+            ItemList[i].SetActive(true);
         }
     }
 
@@ -43,5 +89,13 @@ public class ChoiceItemManager : MonoBehaviour
                 Destroy(ItemList[i].gameObject);
         }
         ItemList.Clear();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() == null)
+            return;
+
+        ItemSpawn();
     }
 }
