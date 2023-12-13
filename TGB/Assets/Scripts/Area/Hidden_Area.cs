c89f2bab148b5d90d4e524640f039b5e7c9a8e48
KDH <ekgns0131@daum.net>
2023-12-14 03:24:07 +0900

    Fix Scene Level Design 맵 확장 및 디자인 수정

--- TGB/Assets/Scripts/Area/Hidden_Area.cs
+++ TGB/Assets/Scripts/Area/Hidden_Area.cs
@@ -12,22 +12,22 @@ public class Hidden_Area : TouchObject
 
     public override void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag != "Player")
+            return;
+
+        foreach (var TR in TRs)
         {
-            foreach(var TR in TRs)
-            {
-                TR.enabled = false;
-            }
+            TR.enabled = false;
         }
     }
     public override void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag != "Player")
+            return;
+
+        foreach (var TR in TRs)
         {
-            foreach (var TR in TRs)
-            {
-                TR.enabled = true;
-            }
-        }
+            TR.enabled = true;
+        }        
     }
 }
