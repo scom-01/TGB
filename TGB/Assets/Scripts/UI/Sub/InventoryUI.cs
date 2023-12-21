827729122fb240737115b6ef5b0e3c0c06fafbb1
KDH <ekgns0131@daum.net>
2023-12-21 22:07:07 +0900

    Fix Scene Animator로 AnimationEvent함수 없이 AudioSource.Play()를 하려고 할 때 Animator의 CullingMode가 Always Animate로 되어있으면 AudioSource의 enable이 true가 되어 원치않는 사운드가 날 수 있다. 기존 - Always Animate 수정 - Cull Update Transforms

--- TGB/Assets/Scripts/UI/Sub/InventoryUI.cs
+++ TGB/Assets/Scripts/UI/Sub/InventoryUI.cs
@@ -1,10 +1,7 @@
 using System;
 using Unity.VisualScripting;
-using UnityEngine.Localization;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
-using TGB.Item;
 
 public enum InventoryUI_State
 {
@@ -13,7 +10,7 @@ public enum InventoryUI_State
 }
 
 namespace TGB.Manager
-{   
+{
     public class InventoryUI : MonoBehaviour
     {
         private Player Player
